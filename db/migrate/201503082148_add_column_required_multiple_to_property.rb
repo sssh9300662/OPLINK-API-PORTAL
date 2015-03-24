e29fd8e16ee90cdc0b79a9056ffdff7820773ae0
Henry Shiu <vita73917@gmail.com>
2015-03-24 22:11:01 +0800

    1. Modify GEM setting 2. Remove reduant migration file

--- db/migrate/201503082148_add_column_required_multiple_to_property.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnProduceTypeToProperties < ActiveRecord::Migration
-  def change
-    t.boolean  "required"
-    t.text     "allowable_values"
-    t.boolean  "allow_multiple"
-    add_column :parameters, :required, :boolean, :after => :description
-  end
-end
